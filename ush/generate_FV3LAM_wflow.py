62313891911f95976ce6e6aebb87b814935d3909
drnimbusrain <dr.nimbusrain@gmail.com>
2025-04-07 21:20:47 -0400

    Bring back 'do_canopy' NL functionality in SRW-App.

--- ush/generate_FV3LAM_wflow.py
+++ ush/generate_FV3LAM_wflow.py
@@ -730,6 +730,12 @@ def setup_fv3_namelist(expt_config,debug):
                 "vsvoo1:0.0", "vsvoo2:0.0", "vsvoo3:0.0", "vsvpo1:0.0", "vsvpo2:0.0",
                 "vsvpo3:0.0", "xopn:0.0", "xylmn:0.0", "*:0.2" ]
         })
+        if DO_AQM_CANOPY:
+            if ( CCPP_PHYS_SUITE == "FV3_GFS_v16" or
+                 CCPP_PHYS_SUITE == "FV3_GFS_v17_p8"):
+                gfs_physics_nml_dict.update({
+                "do_canopy": True
+                }) 
 
     # If UFS_FIRE, activate appropriate flags
     if expt_config['fire'].get('UFS_FIRE'):
