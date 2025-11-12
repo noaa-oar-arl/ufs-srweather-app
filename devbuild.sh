9ed901f1c316ed1893a9f5c7f067952cdf6e9dda
drnimbusrain <dr.nimbusrain@gmail.com>
2025-11-12 16:33:25 -0500

    Exporting conda path explicitly to get environment.

--- devbuild.sh
+++ devbuild.sh
@@ -228,6 +228,7 @@ if [ "${BUILD_CONDA}" = "on" ] ; then
   fi
 
   source ${CONDA_BUILD_DIR}/etc/profile.d/conda.sh
+  export PATH="${CONDA_BUILD_DIR}/bin:${PATH}"
   # Put some additional packages in the base environment on MacOS systems
   if [ "${os}" == "MacOSX" ] ; then
     mamba install -y bash coreutils sed
