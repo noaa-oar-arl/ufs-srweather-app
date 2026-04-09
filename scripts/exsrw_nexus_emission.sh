77654b5ec37ac4905127054e8d596e3f6b474e8a
zmoon <zmoon92@gmail.com>
2026-04-09 11:57:28 -0500

    Bump NEI 2022 version
    and switch to preferred opt style

--- scripts/exsrw_nexus_emission.sh
+++ scripts/exsrw_nexus_emission.sh
@@ -301,7 +301,7 @@ if [ "${NEI2016}" = "TRUE" ]; then
     print_err_msg_exit "${message_txt}"
   fi
 elif [ "${NEI2022}" = "TRUE" ]; then 
-  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src_dir ${FIXemis} --date ${YYYYMMDD} --work_dir ${DATAinput} -v "v2026-03" --metemis none
+  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src-dir ${FIXemis} --date ${YYYYMMDD} --work-dir ${DATAinput} --nei-version "v2026-04" --metemis none
   export err=$?
   if [ $err -ne 0 ]; then
     message_txt="FATAL ERROR Call to python script \"nexus_nei2022_linker.py\" failed."
@@ -316,7 +316,7 @@ elif [ "${NEI2022}" = "TRUE" ]; then
     print_err_msg_exit "${message_txt}"
   fi
 elif [ "${NEI2022_GLOBTEMPO}" = "TRUE" ]; then
-  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src_dir ${FIXemis} --date ${YYYYMMDD} --work_dir ${DATAinput} -v "v2026-03" --metemis none
+  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src-dir ${FIXemis} --date ${YYYYMMDD} --work-dir ${DATAinput} --nei-version "v2026-04" --metemis none
   export err=$?
   if [ $err -ne 0 ]; then
     message_txt="FATAL ERROR Call to python script \"nexus_nei2022_linker.py\" failed."
@@ -331,7 +331,7 @@ elif [ "${NEI2022_GLOBTEMPO}" = "TRUE" ]; then
     print_err_msg_exit "${message_txt}"
   fi
 elif [ "${NEI2022_GLOBTEMPO_METEMIS}" = "TRUE" ]; then
-  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src_dir ${FIXemis} --date ${YYYYMMDD} --work_dir ${DATAinput} -v "v2026-03" --metemis ${METEMIS_SECTOR} --metemis-version "v2026-04"
+  ${USHsrw}/nexus_utils/python/nexus_nei2022_linker.py --src-dir ${FIXemis} --date ${YYYYMMDD} --work-dir ${DATAinput} --nei-version "v2026-04" --metemis ${METEMIS_SECTOR} --metemis-version "v2026-04"
   export err=$?
   if [ $err -ne 0 ]; then
     message_txt="FATAL ERROR Call to python script \"nexus_nei2022_linker.py\" failed."
